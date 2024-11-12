138d13f8a4f05cbbbb5fca6672e1f19735ef71f6
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-11-12 21:43:45 +0500

    build: update ci
    + add nightly workflow

---
+++ build_nightly.ps1
@@ -0,0 +1,13 @@
+# ----------- BepInEx Unity IL2CPP CoreCLR -----------
+$Path = "Release/CinematicUnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR"
+$OutputPath = "$Path/output/BepInEx/plugins/CinematicUnityExplorer"
+
+dotnet build src/CinematicUnityExplorer.sln -c Release_BIE_Unity_Cpp -p:IS_CI=true
+
+New-Item -ItemType Directory -Path $OutputPath -Force
+
+# ILRepack
+lib/ILRepack.exe /target:library /lib:lib/net472/BepInEx/build647+ /lib:lib/net6/ /lib:lib/interop/ /lib:$Path /internalize /out:$OutputPath/CinematicUnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll $Path/CinematicUnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll $Path/mcs.dll $Path/Tomlet.dll
+
+# (cleanup and move files)
+Move-Item -Path $Path/UniverseLib.BIE.IL2CPP.Interop.dll -Destination $OutputPath -Force
