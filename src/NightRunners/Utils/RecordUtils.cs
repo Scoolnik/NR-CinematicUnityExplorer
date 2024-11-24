304c5a38c037ce53413713813ac1de3c9d08f0d7
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-11-24 17:03:30 +0500

    feat: add experimantal record feature

---
+++ src/NightRunners/Utils/RecordUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinematicUnityExplorer.NightRunners.Utils
+{
+    public static class RecordUtils
+    {
+        public static RCC_Recorder.Mode GetMode()
+        {
+            return RCC_SceneManager.instance.recorder.mode;
+        }
+
+        public static void StartRecord()
+        {
+            RCC.StartStopRecord();
+        }
+
+        public static void StopRecord()
+        {
+            RCC.StartStopRecord();
+        }
+
+        public static void StartReplay()
+        {
+            RCC_SceneManager.instance.recorder.carController.externalControllerAI = new RCC_AICarController();
+            RCC.StartStopReplay();
+        }
+
+        public static void StopReplay()
+        {
+            RCC_SceneManager.instance.recorder.carController.externalControllerAI = null;
+            RCC.StartStopReplay();
+        }
+    }
+}
