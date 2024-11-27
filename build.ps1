a086f85e16966b010dc5bcc68b639df155c36f76
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-11-28 00:06:25 +0500

    chore: do not build Release_ML_Cpp_net472 on CI

--- build.ps1
+++ build.ps1
@@ -14,19 +14,6 @@ lib/ILRepack.exe /target:library /lib:lib/net6 /lib:lib/interop /lib:$Path /inte
 
 Move-Item -Path $Path/UniverseLib.ML.IL2CPP.Interop.dll -Destination $Path/output/UserLibs -Force
 
-# ----------- MelonLoader IL2CPP (net472) -----------
-dotnet build src/CinematicUnityExplorer.sln -c Release_ML_Cpp_net472 -p:IS_CI=true
-$Path = "Release/CinematicUnityExplorer.MelonLoader.IL2CPP"
-
-New-Item -ItemType Directory -Path "$Path/output/Mods" -Force
-New-Item -ItemType Directory -Path "$Path/output/UserLibs" -Force
-
-# ILRepack
-lib/ILRepack.exe /target:library /lib:lib/net472 /lib:lib/net35 /lib:lib/unhollowed /lib:$Path /internalize /out:$Path/output/Mods/CinematicUnityExplorer.ML.IL2CPP.dll $Path/CinematicUnityExplorer.ML.IL2CPP.dll $Path/mcs.dll
-
-# (cleanup and move files)
-Move-Item -Path $Path/UniverseLib.IL2CPP.Unhollower.dll -Destination $Path/output/UserLibs -Force
-
 # ----------- BepInEx Unity IL2CPP CoreCLR -----------
 dotnet build src/CinematicUnityExplorer.sln -c Release_BIE_Unity_Cpp -p:IS_CI=true
 $Path = "Release/CinematicUnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR"
