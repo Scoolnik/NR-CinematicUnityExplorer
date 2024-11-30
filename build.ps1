66332dbbf168fd52e92f4cdc143515b71b458ffb
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2024-11-30 15:02:13 +0500

    chore: temporary disable melon build

--- build.ps1
+++ build.ps1
@@ -3,16 +3,16 @@ cd UniverseLib
 cd ..
 
 # ----------- MelonLoader IL2CPP CoreCLR (net6) -----------
-dotnet build src/CinematicUnityExplorer.sln -c Release_ML_Cpp_CoreCLR -p:IS_CI=true
-$Path = "Release\CinematicUnityExplorer.MelonLoader.IL2CPP.CoreCLR"
+# dotnet build src/CinematicUnityExplorer.sln -c Release_ML_Cpp_CoreCLR -p:IS_CI=true
+# $Path = "Release\CinematicUnityExplorer.MelonLoader.IL2CPP.CoreCLR"
 
-New-Item -ItemType Directory -Path "$Path/output/Mods" -Force
-New-Item -ItemType Directory -Path "$Path/output/UserLibs" -Force
+# New-Item -ItemType Directory -Path "$Path/output/Mods" -Force
+# New-Item -ItemType Directory -Path "$Path/output/UserLibs" -Force
 
-# ILRepack
-lib/ILRepack.exe /target:library /lib:lib/net6 /lib:lib/interop /lib:$Path /internalize /out:$Path/output/Mods/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.dll $Path/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.dll $Path/mcs.dll
+# # ILRepack
+# lib/ILRepack.exe /target:library /lib:lib/net6 /lib:lib/interop /lib:$Path /internalize /out:$Path/output/Mods/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.dll $Path/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.dll $Path/mcs.dll
 
-Move-Item -Path $Path/UniverseLib.ML.IL2CPP.Interop.dll -Destination $Path/output/UserLibs -Force
+# Move-Item -Path $Path/UniverseLib.ML.IL2CPP.Interop.dll -Destination $Path/output/UserLibs -Force
 
 # ----------- BepInEx Unity IL2CPP CoreCLR -----------
 dotnet build src/CinematicUnityExplorer.sln -c Release_BIE_Unity_Cpp -p:IS_CI=true
